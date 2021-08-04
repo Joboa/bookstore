db2703d4370f603d56034d76aa87a0a772a7087c
John Boamah <joboa2015@gmail.com>
2021-08-04 11:40:57 +0000

    Add postgresql as default database

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vzbn0w$gzr$292otsi$7f5f+mve35=optm&l62vl^#pi2(1^72
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'bookstore_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
 

fc17ebdbffbcecb4fbf353dcdef02489433257cf
John Boamah <joboa2015@gmail.com>
2021-08-05 15:37:57 +0000

    Add custom users

---
+++ users/tests.py
@@ -0,0 +1,33 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+
+class CustomUserTests(TestCase):
+  def test_create_user(self):
+    User = get_user_model()
+    user = User.objects.create_user(
+        username='boamah',
+        email='boamah@joboadevs.com',
+        password='OneDayIWllBeAProfessionalDeveloper'
+        )
+
+    self.assertEqual(user.username, 'boamah')
+    self.assertEqual(user.email, 'boamah@joboadevs.com')
+    self.assertTrue(user.is_active)
+    self.assertFalse(user.is_staff)
+    self.assertFalse(user.is_superuser)
+
+
+  def test_create_superuser(self):
+    User = get_user_model()
+    admin_user = User.objects.create_superuser(
+        username='superadmin',
+        email='superadmin@joboadevs.com',
+        password='ItsWell'
+        )
+
+    self.assertEqual(admin_user.username, 'superadmin')
+    self.assertEqual(admin_user.email, 'superadmin@joboadevs.com')
+    self.assertTrue(admin_user.is_active)
+    self.assertTrue(admin_user.is_staff)
+    self.assertTrue(admin_user.is_superuser)

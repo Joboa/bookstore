4e2d1139b46b27b193589c94199035fb099db542
John Boamah <joboa2015@gmail.com>
2021-08-09 12:08:49 +0000

    Add new test for allauth account: login, logout and signup

--- pages/tests.py
+++ pages/tests.py
@@ -4,47 +4,48 @@ from django.http import response
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 
+
 class HomePageTests(SimpleTestCase):
 
-  def setUp(self):
-    url = reverse('home')
-    self.response = self.client.get('/')
+    def setUp(self):
+        url = reverse('home')
+        self.response = self.client.get('/')
 
-  def test_homepage_status_code(self):
-    self.assertEqual(self.response.status_code, 200)
+    def test_homepage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
 
-  def test_homepage_template(self):
-    self.assertTemplateUsed(self.response, 'home.html')
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.response, 'home.html')
 
-  def test_homepage_contains_correct_html(self):
-    self.assertContains(self.response, 'Home Page')
+    def test_homepage_contains_correct_html(self):
+        self.assertContains(self.response, 'Home Page')
 
-  def test_homepage_does_not_contain_incorrect_html(self):
-    self.assertNotContains(self.response, 'This is John going Pro!')
+    def test_homepage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, 'This is John going Pro!')
 
-  def test_homepage_url_resolves_homepageview(self):
-    view = resolve('/')
-    self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
-    
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve('/')
+        self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
+
 
 class AboutPageTests(SimpleTestCase):
 
-  def setUp(self):
-    url = reverse('about')
-    self.response = self.client.get(url)
+    def setUp(self):
+        url = reverse('about')
+        self.response = self.client.get(url)
 
-  def test_aboutpage_status_code(self):
-    self.assertAlmostEqual(self.response.status_code, 200)
+    def test_aboutpage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
 
-  def test_aboutpage_template(self):
-    self.assertTemplateUsed(self.response, 'about.html')
+    def test_aboutpage_template(self):
+        self.assertTemplateUsed(self.response, 'about.html')
 
-  def test_aboutpage_contains_correct_html(self):
-    self.assertContains(self.response, 'About Page')
+    def test_aboutpage_contains_correct_html(self):
+        self.assertContains(self.response, 'About Page')
 
-  def test_aboutpage_does_not_contains_incorrect_html(self):
-    self.assertNotContains(self.response, 'Just added crispy forms!')
+    def test_aboutpage_does_not_contains_incorrect_html(self):
+        self.assertNotContains(self.response, 'Just added crispy forms!')
 
-  def test_aboutpage_url_resolves_aboutpageview(self):
-    view = resolve('/about/')
-    self.assertEqual(view.func.__name__, AboutPageView.as_view().__name__)
+    def test_aboutpage_url_resolves_aboutpageview(self):
+        view = resolve('/about/')
+        self.assertEqual(view.func.__name__, AboutPageView.as_view().__name__)
